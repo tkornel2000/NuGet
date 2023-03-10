736acc9e86ab37ab9e48f2cd5d23a2a741877018
tkornel2000 <tkornel.takacs2000@gmail.com>
2023-03-10 17:17:16 +0100

    Fix the claculate

---
+++ AppLogger/AppLogger/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogger
+{
+    internal class Calculator
+    {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+        public double Sub(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public double Div(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
