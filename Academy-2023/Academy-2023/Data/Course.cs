575f31e4882c2f9990118124df2d8ca05d27a1a7
tkornel2000 <71631284+tkornel2000@users.noreply.github.com>
2023-03-10 17:18:13 +0100

    Delete Academy-2023 directory

--- Academy-2023/Academy-2023/Data/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Academy_2023.Data
-{
-    public class Course
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-    }
-}
